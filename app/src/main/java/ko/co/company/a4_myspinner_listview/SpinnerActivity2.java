370440911ca3f76d68f8fe22d78fbff4587a015f
Hwang eunyoung <hhy0802@naver.com>
2023-05-01 15:02:50 +0900

    4장 안드로이드 spinner listview

---
+++ app/src/main/java/ko/co/company/a4_myspinner_listview/SpinnerActivity2.java
@@ -0,0 +1,14 @@
+package ko.co.company.a4_myspinner_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpinnerActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner2);
+    }
+}
